9ea4a1ce54450a5ede71bc80a644fb8ba0c06ab6
jgrissom <jgrissom91@gmail.com>
2023-09-26 18:33:20 -0500

    [2.02] Create role

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -21,5 +21,21 @@ namespace JWTExample.Controllers
     [SwaggerOperation(summary: "Return all roles", null)]
     [SwaggerResponse(200, "Success", typeof(RoleDTO))]
     public IActionResult Get() => Ok(_roleManager.Roles.OrderBy(r => r.Name).Select(r => new RoleDTO { Id = r.Id, Name = r.Name }));
+    [HttpPost]
+    [Route("create")]
+    [SwaggerOperation(summary: "Create role", null)]
+    [SwaggerResponse(204, "Role created", null)]
+    public async Task<IActionResult> Create([FromBody] CreateRole model)
+    {
+      // Check if role already exists
+      var role = await _roleManager.FindByNameAsync(model.RoleName);
+      if (role != null)
+        return Conflict(new { Message = $"{model.RoleName} is a duplicate Role" });
+
+      var result = await _roleManager.CreateAsync(new IdentityRole(model.RoleName));
+      if (!result.Succeeded)
+        return StatusCode(StatusCodes.Status500InternalServerError, new { Message = "Failed to create role" });
+      return NoContent();
+    }
   }
 }
