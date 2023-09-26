8f4bda95765cc176b49d30e0deb7ab5e5f6af0ee
jgrissom <jgrissom91@gmail.com>
2023-09-26 18:22:37 -0500

    [1.16] User login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using JWTSwagger.Authentication;
 using Swashbuckle.AspNetCore.Annotations;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace JWTSwagger.Controllers
 {
@@ -12,10 +16,12 @@ namespace JWTSwagger.Controllers
   {
     // access UserManager w/ dependecy injection
     private readonly UserManager<ApplicationUser> _userManager;
-    public UserController(UserManager<ApplicationUser> userManager)
+    private readonly IConfiguration _configuration;
+    public UserController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
     {
       _userManager = userManager;
-     }
+      _configuration = configuration;
+    }
 
     [HttpGet]
     [SwaggerOperation(summary: "Return all users", null)]
@@ -52,6 +58,47 @@ namespace JWTSwagger.Controllers
         return StatusCode(StatusCodes.Status500InternalServerError);
 
       return NoContent();
+    }
+    [HttpPost]
+    [Route("login")]
+    [SwaggerOperation(summary: "User login", null)]
+    [SwaggerResponse(200, "Success", typeof(UserTokenDTO))]
+    public async Task<IActionResult> Login([FromBody] UserLogin model)
+    {
+      var user = await _userManager.FindByNameAsync(model.Username);
+      if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+      {
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        var authClaims = new List<Claim>
+        {
+          new Claim(JwtRegisteredClaimNames.Email, user.Email),
+          new Claim(JwtRegisteredClaimNames.Jti, user.Id),
+          new Claim("username", user.UserName),
+        };
+
+        foreach (var userRole in userRoles)
+        {
+          authClaims.Add(new Claim("roles", userRole));
+        }
+
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+        var token = new JwtSecurityToken(
+          issuer: _configuration["JWT:ValidIssuer"],
+          audience: _configuration["JWT:ValidAudience"],
+          expires: DateTime.Now.AddHours(3),
+          claims: authClaims,
+          signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return Ok(new UserTokenDTO
+        {
+          Token = new JwtSecurityTokenHandler().WriteToken(token),
+          Expiration = Convert.ToString(token.ValidTo)
+        });
+      }
+      return Unauthorized(new { Message = "Login falied" });
     }
     [HttpPost]
     [Route("resetpassword")]
