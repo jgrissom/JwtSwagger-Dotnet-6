53927b893831a2c946cd2bece83150c43f6df0cd
jgrissom <jgrissom91@gmail.com>
2023-09-26 17:18:18 -0500

    [1.10] User Register

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -17,5 +17,30 @@ namespace JWTSwagger.Controllers
 
     [HttpGet]
     public IActionResult Get() => Ok(_userManager.Users);
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] UserRegister model)
+    {
+      // check for existence of user
+      var userExists = await _userManager.FindByEmailAsync(model.Email);
+      if (userExists != null) // duplicate email
+        return Conflict(new { message = $"{model.Email} is a duplicate Email" });
+      userExists = await _userManager.FindByNameAsync(model.Username);
+      if (userExists != null) // duplicate username
+        return Conflict(new { message = $"{model.Username} is a duplicate Username" });
+
+      ApplicationUser user = new ApplicationUser()
+      {
+        Email = model.Email,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        UserName = model.Username
+      };
+      var result = await _userManager.CreateAsync(user, model.Password);
+      if (!result.Succeeded)
+        return StatusCode(StatusCodes.Status500InternalServerError);
+
+      return NoContent();
+    }
   }
 }
